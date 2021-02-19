33bc9c891a8499d1efa107127960df82fa48ca0e
JuvenileTalk9 <prdmq578@gmail.com>
2021-02-19 09:28:27 +0900

    02_画像認識

--- 02_画像認識/cpp/vehicle_recognition.cpp
+++ 02_画像認識/cpp/vehicle_recognition.cpp
@@ -1,23 +1,11 @@
-# 画像認識
+#include <iostream>
 
-ここではCNNなどの画像認識を事前学習モデルで実行する手順を示します。今回は車判定モデルを使います。
-
-使用する事前学習モデルは以下の車判定モデルを使用します。
-
-[https://github.com/openvinotoolkit/open_model_zoo/blob/master/models/intel/vehicle-attributes-recognition-barrier-0042/description/vehicle-attributes-recognition-barrier-0042.md](https://github.com/openvinotoolkit/open_model_zoo/blob/master/models/intel/vehicle-attributes-recognition-barrier-0042/description/vehicle-attributes-recognition-barrier-0042.md)
-
-モデルはOpenVINOのモデルダウンローダを使用します。
-
-```sh
-$ cd /opt/intel/openvino_2020.4.287/deployment_tools/open_model_zoo/tools/downloader
-$ sudo python3 downloader.py --name vehicle-attributes-recognition-barrier-0042 --precisions FP32
-```
-
-## Python
+#include <inference_engine.hpp>
 
 
+int main(int argc, char** argv) {
 
-## C++
+    Core ie;
+    CNNNetwork network = ie.ReadNetwork(FLAGS_m);
 
-
-[目次へ戻る](https://github.com/JuvenileTalk9/OpenVINO)
+}
